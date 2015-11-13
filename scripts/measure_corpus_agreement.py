b5087707a6c49ac09aeee134d33aafb2439c2565
Wladimir Sidorenko <wlsidorenko@gmail.com>
2015-11-13 18:16:58 +0100

    Update measure_corpus_agreement.py

--- scripts/measure_corpus_agreement.py
+++ scripts/measure_corpus_agreement.py
@@ -19,7 +19,7 @@ corpus/annotator-1/markables/ corpus/annotator-2/markables/
 
 LICENSE (modified MIT):
 =======================
-Copyright (c) 2014-2015, ANONYMOUS
+Copyright (c) 2014-2015, Uladzimir Sidarenka <sidarenk at uni dash potsdam dot de>
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
