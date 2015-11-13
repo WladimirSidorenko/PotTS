e8d519c8f9148e3281763b98d425beba27f170d7
Wladimir Sidorenko <wlsidorenko@gmail.com>
2015-11-13 18:18:02 +0100

    Update alt_fio.py

--- scripts/alt_fio.py
+++ scripts/alt_fio.py
@@ -12,7 +12,8 @@ AltFileOutput - class for interactive reading of input files
 
 LICENSE (modified MIT):
 =======================
-Copyright (c) 2014-2015, ANONYMOUS
+Copyright (c) 2014-2015, Uladzimir Sidarenka <sidarenk at uni dash potsdam dot de>
+
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
